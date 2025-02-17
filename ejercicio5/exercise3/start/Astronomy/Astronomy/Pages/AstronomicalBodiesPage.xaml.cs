902f9b76e10ce80e9f0c8455f5a810b3c641fc4c
HugoNeiraColilla29 <hugo.neira@educa.madrid.org>
2025-02-17 19:11:46 +0100

    ejercicio5pt3

--- ejercicio5/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
+++ ejercicio5/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -4,7 +4,24 @@ public partial class AstronomicalBodiesPage : ContentPage
 {
 	public AstronomicalBodiesPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
+
+    }
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+
+            // this is a custom function to update the UI immediately
+            UpdateAstroBodyUI(astroName);
+        }
     }
 }
